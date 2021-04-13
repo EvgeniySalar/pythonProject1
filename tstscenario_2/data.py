2978ff8b26ca808d25b8825c896ba6cd9835c888
EvgeniySalar <74773803+EvgeniySalar@users.noreply.github.com>
2021-04-13 22:10:11 +0300

    Add files via upload

---
+++ tstscenario_2/data.py
@@ -0,0 +1,5 @@
+MAIN_PAGE_URL = "https://www.booking.com/index.html?label=" \
+                "gen173nr-1DCAEoggI46AdIM1gEaOkBiAEBmAEhuAEXyAEM2AED6AEBiAIBqAIDuALh7-" \
+                "uCBsACAdICJGYxZDgxZThiLTZjZDQtNDk1Mi05ZDcwLWQwN2Q4OTZjODA3NtgCBOACAQ&sid=" \
+                "28deaec43840af3789c593babab79aa1&lang=en-us&sb_price_type=total&soz=" \
+                "1&lang_click=other;cdl=ru;lang_changed=1"
